49ed03a7cda4849cb05d3ce4ae3765c9c1e046af
apricotxapril <auay.smiley@icloud.com>
2022-07-04 12:32:38 +0700

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="mx-20 my-10">
+    <div class="container flex justify-between">
+      <a class="text-3xl font-bold" href=""><span>DearDiary.</span></a>
+      <div>
+        <ul class="flex flex-row gap-10">
+          <li><a class="text-xl font-medium hover:text-emerald-700" href="">Features</a></li>
+          <li><a class="text-xl font-medium hover:text-emerald-700" href="">Prices</a></li>
+          <li><a class="text-xl font-medium hover:text-emerald-700" href="">About</a></li>
+          <li><a class="text-xl font-medium hover:text-emerald-700" href="">Reviews</a></li>
+          <li><a class="text-xl font-medium hover:text-emerald-700" href="">Contacts</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
