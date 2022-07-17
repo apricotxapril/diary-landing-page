ffc889a9c18e727419610db120d0816cedf06810
Sorawong L <Sorawong2543@gmail.com>
2022-07-17 08:46:03 +0700

    responsive `<Header />`

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,14 @@
 <template>
   <div
-    class="px-8 flex flex-col space-y-6 bg-gradient-to-r from-green-200 to-blue-300 relative pt-14 pb-44 sm:pb-0"
+    class="px-8 bg-gradient-to-r from-green-200 to-blue-300 relative pt-14 pb-44 sm:py-30 sm:flex sm:flex-row sm:justify-center sm:items-center"
   >
-    <div class="sm:w-1/2">
-      <article class="text-center m-auto space-y-3">
-        <h1 class="text-3xl font-bold leading-10">
+    <div
+      data-aos="fade-right"
+      data-aos-duration="3000"
+      class="z-10 flex flex-col space-y-6 sm:justify-center sm:flex-[0.45] sm:space-y-8 md:space-y-11 lg:space-y-14"
+    >
+      <article class="z-10 text-center mx-auto space-y-3 sm:text-start sm:space-y-4">
+        <h1 class="text-3xl font-bold leading-10 md:text-4xl lg:text-5xl">
           Take a note everywhere
           <br />
           with
@@ -12,24 +16,38 @@
             >DearDiary</span
           >
         </h1>
-        <p class="text-gray-600">
+        <p class="text-gray-600 sm:text-xs lg:text-lg">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Massa tempor nec feugiat nisl pretium fusce id.
         </p>
       </article>
+      <button
+        class="z-10 text-md text-white font-medium bg-gradient-to-r from-emerald-500 to-blue-500 px-5 py-3 rounded-lg lg:text-xl lg:font-bold lg:py-4 sm:max-w-[14rem]"
+      >
+        Get Started
+      </button>
     </div>
-    <button
-      class="text-md text-white font-medium bg-gradient-to-r from-emerald-500 to-blue-500 px-5 py-3 rounded-lg"
-    >
-      Get Started
-    </button>
-    <div class="hidden sm:block sm:w-1/2">
-      <div class="animate-floating">
-        <img src="@/assets/diary.png" alt="" />
+    <div class="hidden sm:block sm:flex-[0.4] sm:z-10">
+      <div class="animate-fading max-w-lg">
+        <img
+          class="animate-imageFloating"
+          src="@/assets/diary.png"
+          alt="image"
+          width="2800"
+          height="2800"
+        />
       </div>
     </div>
     <div
-      class="w-full h-[12rem] sm:h-[20rem] absolute left-0 -bottom-2 bg-gradient-to-b from-transparent to-white"
+      class="w-full h-[18rem] lg:h-[25rem] absolute left-0 -bottom-2 bg-gradient-to-b from-transparent to-white"
     />
   </div>
 </template>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import AOS from 'aos'
+
+onMounted(() => {
+  AOS.init()
+})
+</script>
