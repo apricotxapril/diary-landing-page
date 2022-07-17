c26d5580dd980232e03d644fe68fe9dd16e27964
Sorawong L <Sorawong2543@gmail.com>
2022-07-17 17:30:22 +0700

    responsive `<About />`

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,32 +1,50 @@
 <template>
-  <div data-aos="fade-up" data-aos-duration="3000" class="flex h-96 mx-40 mt-10 mb-20 px-10 py-5">
-    <div class="w-1/2 bg-gray-300 rounded-lg"></div>
-    <div class="w-1/2">
-      <h1 class="text-4xl text-start font-medium pt-20 px-20">Plan</h1>
-      <p class="text-xl text-start font-medium pt-8 px-20">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, esse amet quaerat dicta
-        aut earum iste optio at. Pariatur, voluptas.
-      </p>
+  <div class="space-y-16">
+    <div
+      data-aos="fade-up"
+      data-aos-duration="3000"
+      class="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-8 sm:px-10 sm:justify-center md:space-x-16"
+    >
+      <div class="w-3/4 h-[15rem] bg-gray-300 rounded-lg sm:flex-1 md:max-w-sm"></div>
+      <article class="text-start font-medium space-y-2 w-3/4 sm:flex-1 md:max-w-sm">
+        <h1 class="text-2xl md:text-3xl">Plan</h1>
+        <p class="text-md font-light text-gray-500 md:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, esse amet quaerat
+          dicta aut earum iste optio at. Pariatur, voluptas.
+        </p>
+      </article>
     </div>
-  </div>
-  <div data-aos="fade-up" data-aos-duration="3000" class="flex h-96 mx-40 mt-10 mb-20 px-10 py-5">
-    <div class="w-1/2 bg-gray-300 rounded-lg order-last"></div>
-    <div class="w-1/2">
-      <h1 class="text-4xl text-start font-medium pt-20 px-20">Write</h1>
-      <p class="text-xl text-start font-medium pt-8 px-20">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim expedita explicabo iste dolor
-        nisi molestiae, molestias nulla. Officia, aperiam voluptas.
-      </p>
+    <div
+      data-aos="fade-up"
+      data-aos-duration="3000"
+      class="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-8 sm:px-10 sm:justify-center md:space-x-16"
+    >
+      <article
+        class="order-2 text-start font-medium space-y-2 w-3/4 sm:order-1 sm:flex-1 md:max-w-sm"
+      >
+        <h1 class="text-2xl md:text-3xl">Write</h1>
+        <p class="text-md font-light text-gray-500 md:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, esse amet quaerat
+          dicta aut earum iste optio at. Pariatur, voluptas.
+        </p>
+      </article>
+      <div
+        class="order-1 w-3/4 h-[15rem] bg-gray-300 rounded-lg sm:order-2 sm:flex-1 md:max-w-sm"
+      ></div>
     </div>
-  </div>
-  <div data-aos="fade-up" data-aos-duration="3000" class="flex h-96 mx-40 mt-10 px-10 py-5">
-    <div class="w-1/2 bg-gray-300 rounded-lg"></div>
-    <div class="w-1/2">
-      <h1 class="text-4xl text-start font-medium pt-20 px-20">List</h1>
-      <p class="text-xl text-start font-medium pt-8 px-20">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit magnam velit numquam
-        doloremque expedita maxime, deleniti perspiciatis sint quo libero.
-      </p>
+    <div
+      data-aos="fade-up"
+      data-aos-duration="3000"
+      class="flex flex-col items-center space-y-4 sm:flex-row sm:space-x-8 sm:px-10 sm:justify-center md:space-x-16"
+    >
+      <div class="w-3/4 h-[15rem] bg-gray-300 rounded-lg sm:flex-1 md:max-w-sm"></div>
+      <article class="text-start font-medium space-y-2 w-3/4 sm:flex-1 md:max-w-sm">
+        <h1 class="text-2xl md:text-3xl">List</h1>
+        <p class="text-md font-light text-gray-500 md:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, esse amet quaerat
+          dicta aut earum iste optio at. Pariatur, voluptas.
+        </p>
+      </article>
     </div>
   </div>
 </template>
