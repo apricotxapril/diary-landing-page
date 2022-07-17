ff93abdc1e7a28fca22bf16e42572c96253d1c73
Sorawong L <Sorawong2543@gmail.com>
2022-07-17 08:42:43 +0700

    added `aos` library to main

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './scss/main.scss'
+import 'aos/dist/aos.css'
 
 createApp(App).use(router).mount('#app')
