713d818aa97ab5dc62edf9f6ed7eb7d24b2abc23
Sorawong L <Sorawong2543@gmail.com>
2022-07-17 08:41:31 +0700

    added `floating` and `fading` animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,21 @@ module.exports = {
       },
       keyframes: {
         floating: {
-          '0%': { transform: 'translateY(-6%)' },
-          '33%': { transform: 'translateY(-4%)' },
-          '66%': { transform: 'translateY(-2%)' },
-          '100%': { transform: 'translateY(0)' },
+          '0%': { transform: 'translateY(-3%)' },
+          '33%': { transform: 'translateY(-1%)' },
+          '66%': { transform: 'translateY(1%)' },
+          '100%': { transform: 'translateY(3%)' },
+        },
+        fading: {
+          '0%': { opacity: '0' },
+          '33%': { opacity: '0.33' },
+          '66%': { opacity: '0.66' },
+          '100%': { opacity: '1' },
         },
       },
       animation: {
-        floating: 'floating 3s linear infinite alternate',
+        imageFloating: 'floating 3s linear infinite alternate',
+        fading: 'fading 1s',
       },
     },
   },
